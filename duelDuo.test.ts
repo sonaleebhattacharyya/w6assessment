41702cdba9403bf1a357a23b033f24d9ff498eb1
Sonalee Bhattacharyya <sonaleebhattacharyya@gmail.com>
2022-12-18 08:27:48 -0600

    updates

--- duelDuo.test.ts
+++ duelDuo.test.ts
@@ -4,16 +4,24 @@ require('chromedriver')
 const driver = new Builder().withCapabilities(Capabilities.chrome()).build()
 
 beforeEach(async () => {
-   await driver.get('http://127.0.0.1:5500/public/')
+ await driver.get('http://127.0.0.1:5500/public/')
 })
 
 afterAll(async () => {
     driver.quit()
 })
-
+// First Test (included in the original file): Test that title shows up when page loads. 
 test('Title shows up when page loads', async () => {
     const title = await driver.findElement(By.id('title'))
     const displayed = await title.isDisplayed()
     expect(displayed).toBe(true)
 })
+// Second Test: Let's check that clicking the Draw button displays the div with id = "choices""
 
+test('clicking Draw button displays div with id = choices', async () => {
+  const draw = await driver.findElement(By.id('draw'))
+  await draw.click()
+  const click = await driver.findElement(By.id('choices'))
+  const displayed = await click.isDisplayed()
+  expect(displayed).toBe(true)
+ })
--- functions.test.ts
+++ functions.test.ts
@@ -1,9 +1,18 @@
 import {expect, jest, test} from '@jest/globals';
 const {shuffleArray} = require('./utils')
+// Test checks whether the shuffled array is the same length as the original. So if you input 
+// [1, 2, 3], and shuffle it, the shuffled array should also have length 3. 
 
 describe('shuffleArray should', () => {
     test('be of the same length as original',()=> {
         let newarray = shuffleArray([1, 2, 3])
         expect(newarray.length).toBe(3) 
     } )
+
+    test('should be of the same type', ()=> {
+        let newarray = shuffleArray([])
+        expect(Array.isArray(newarray)).toBe(true)
+    } )
 })
+
+
